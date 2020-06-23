de73f2cff938517a85bf9cb1bfc40e3c9e902328
Gold7474 <58492172+Gold7474@users.noreply.github.com>
2020-06-23 16:06:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', message=>{
             break;
         case 'buttshot':   
             message.guild.members.get('268856992464896000').setNickname("ButtShot");
-            message.channel.send('Darkshot? More like ButtShot!');
+            message.channel.send('DarkShot? More like ButtShot!');
             break;
         }
     }
